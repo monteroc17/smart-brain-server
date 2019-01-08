ca3f8ccdce8e41e811bd4995829598ec5a9ee08f
Daniel Montero <d.montero95@gmail.com>
2019-01-08 13:03:01 -0600

    db connection

--- server.js
+++ server.js
@@ -11,11 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'guarded-retreat-90274',
-    // port: '5433',
-    user : 'postgres',
-    password : 'damoca12',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
